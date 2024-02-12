a6d9df688b24cf203ed0b2ef98af534f22745467
Houmame LAZAR <houms937@gmail.com>
2024-02-12 08:27:24 +0100

    adding de serve media on upload directory TODO: securize the protected files

--- src/media/media.module.ts
+++ src/media/media.module.ts
@@ -3,10 +3,20 @@ import { MediaService } from './media.service';
 import { MediaController } from './media.controller';
 import { MediaProvider } from './providers/media.provider';
 import { DatabaseModule } from '../database/database.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   controllers: [MediaController],
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'uploads'),
+      serveStaticOptions: {
+        index: false,
+      },
+    }),
+  ],
   providers: [MediaProvider, MediaService],
 })
 export class MediaModule {}
