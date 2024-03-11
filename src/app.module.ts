357a36bc7465d82292dcb1fc6daee3487ed8196c
Houmame LAZAR <houms937@gmail.com>
2024-03-11 11:20:23 +0100

    JOINUS-35-back-ratelimit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MediaModule } from './media/media.module';
 import { PermissionsModule } from './permissions/permissions.module';
 import config from './config/config';
 import { User } from './users/entities/user.entity';
 import { Media } from './media/entities/media.entity';
 import { Permission } from './permissions/entities/permission.entity';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -17,10 +19,35 @@ import { Permission } from './permissions/entities/permission.entity';
       load: [config],
     }),
     DatabaseModule.forRoot([User, Media, Permission]),
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule], // Assurez-vous que ConfigModule est importé
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        configService.get('NODE_ENV') === 'prod'
+          ? [
+              {
+                ttl: configService.get<number>('THROTTLE_TTL') ?? 6e4,
+                limit: configService.get<number>('THROTTLE_LIMIT') ?? 10,
+              },
+            ]
+          : [
+              {
+                ttl: 0,
+                limit: 10,
+              },
+            ],
+    }),
     AuthModule,
     UsersModule,
     MediaModule,
     PermissionsModule,
+  ],
+
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
   ],
 })
 export class AppModule {}
